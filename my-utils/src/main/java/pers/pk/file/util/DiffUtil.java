db39e4fdaaa5e2329638c69229e53ea60d25c7a4
pankekp <pankekp@163.com>
2021-01-16 16:13:11 +0800

    add diff util

---
+++ my-utils/src/main/java/pers/pk/file/util/DiffUtil.java
@@ -0,0 +1,35 @@
+package pers.pk.file.util;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class DiffUtil {
+
+    /**
+     * return elements of list1 that are not in list2
+     */
+    public static <T> List<T> getDiffs(List<T> list1, List<T> list2) {
+
+        Map<T, Integer> map = new HashMap<>();
+
+        for (T t : list1) {
+            map.put(t, 1);
+        }
+
+        for (T t : list2) {
+            map.putIfAbsent(t, 2);
+        }
+
+        List<T> result = new LinkedList<>();
+
+        for (T key : map.keySet()) {
+            if (map.get(key) == 2) {
+                result.add(key);
+            }
+        }
+
+        return result;
+    }
+}
